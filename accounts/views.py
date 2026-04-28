9eb3998549d97c09fbcb42d71ccc505d2ee8f76b
picker770 <arehman897@gmail.com>
2026-04-28 10:03:50 +0100

    Add registration view and profile view with login decorator

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def register(request):
+    """Handle user registration"""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+        return render(request, 'accounts/register.html', {'form': form})
+    
+@login_required
+def profile(request):
+        """Display user profile page"""
+        return render(request, 'accounts/profile.html')
