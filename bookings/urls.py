819eddb8111f01b861b3502aca503d9f870c6c4c
picker770 <arehman897@gmail.com>
2026-04-29 12:29:47 +0100

    add URLs for create, my_bookings, cancel and success pages

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('create/', views.create_booking, name='create_booking'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'),
+    path('cancel/<int:booking_id>/', views.cancel_booking, name="cancel_booking"),
+    path('success/<int:booking_id>/', views.booking_success, name="booking_success"),
 ]
