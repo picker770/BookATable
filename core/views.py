fb2560a9940f0064688b6c53e080cb100885bed3
picker770 <arehman897@gmail.com>
2026-05-09 16:06:57 +0100

    remove custom 404 handler, use Django automatic template

--- core/views.py
+++ core/views.py
@@ -6,6 +6,4 @@ def home(request):
     """Homepage view"""
     return render(request, 'core/home.html')
 
-def custom_404(request, exception):
-    """Custom 404 error handler"""
-    return render(request, '404.html', status=404)
+
